9c17ee86732c1dba94ab1ddf112675b5ae597460
Vineet Pandey <vineet1992.vp@gmail.com>
2021-08-09 16:53:55 +0530

    add wolf1

---
+++ src/Wolf1.java
@@ -0,0 +1,13 @@
+public class Wolf1 {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
