cfebfee826ebab0eb2b15d974cd06e7f4dbb7c8b
Vineet Pandey <vineet1992.vp@gmail.com>
2021-08-09 17:06:34 +0530

    bug fix

--- src/Kitten.java
+++ src/Kitten.java
@@ -3,4 +3,8 @@ public class Kitten {
     public static void main(String[] args) {
         System.out.println("kitten");
     }
+
+    public void bugFix() {
+        System.out.println("bugfix");
+    }
 }
